dd7b6fdb96c9f3e919dfcc5585abba618d6629ee
anelisa <anelisa.silva25@gmail.com>
2020-07-28 21:26:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Meu curriculo</h1>`;
+
+const title = document.querySelector('h1');
+title.textContent = 'Modifiquei aqui, mas continua sendo meu curriculo';
